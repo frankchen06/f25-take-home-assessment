110168b207b4a7a40b0e1e284de161a423fa1337
frankchen06 <100103672+frankchen06@users.noreply.github.com>
2025-06-22 20:02:09 -0400

    Implemented POST /weather endpoint

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,16 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict, Any, Optional
-import uvicorn
+import uvicorn   
+import os
+import secrets
+import requests
+from dotenv import load_dotenv
 
 app = FastAPI(title="Weather Data System", version="1.0.0")
+
+load_dotenv()
+WEATHERSTACK_API_KEY = os.environ.get("WEATHERSTACK_API_KEY") # Get API key from the .env in the backend directory
 
 app.add_middleware(
     CORSMiddleware,
@@ -34,7 +41,40 @@ async def create_weather_request(request: WeatherRequest):
     3. Stores combined data with unique ID in memory
     4. Returns the ID to frontend
     """
-    pass
+    
+    # First did some error handling below for validaing input and if the API key is set 
+    if not WEATHERSTACK_API_KEY:
+        raise HTTPException(status_code=500, detail="WeatherStack API key not configured") 
+
+    if not request.date or not request.location:
+        raise HTTPException(status_code=400, detail="Missing date/location in request") 
+    
+    #Code below calls the API and error handles accordingly 
+    api_params = {
+        "access_key": WEATHERSTACK_API_KEY,
+        "query": request.location,
+    }
+    try:
+        api_response = requests.get("http://api.weatherstack.com/current", params=api_params, timeout = 5)
+        api_data = api_response.json()
+    except Exception:
+        raise HTTPException(status_code=502, detail="Failed to contact API")
+
+    if "error" in api_data:
+        raise HTTPException(status_code=400, detail="Invalid location/API error")
+    
+    # Store data with a unqiue ID by using secrets
+    entry_id = secrets.token_urlsafe(12)
+
+    weather_storage[entry_id] = {
+        "date": request.date,
+        "location": request.location,
+        "notes": request.notes,
+        "weather": api_data,
+    }
+    # Return ID to frontend
+    return WeatherResponse(id=entry_id)
+
 
 @app.get("/weather/{weather_id}")
 async def get_weather_data(weather_id: str):
